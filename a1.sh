689652ba6d72ddfbe319a854d15f8992f38f859c
Mark Jervelund <mark@jervelund.com>
2018-09-24 17:32:02 +0200

    Trying to automate building.

--- a1.sh
+++ a1.sh
@@ -3,7 +3,9 @@ export MOD_INSTALL_PREFIX=/mnt/nfs/Shared/jlandersen/shared
 export PATH=$MOD_INSTALL_PREFIX/bin:$PATH
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$MOD_INSTALL_PREFIX/lib/pkgconfig
 export LD_LIBRARY_PATH=/mnt/nfs/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
+
+
 cd /home/imada/mjerv15/courses/DM840/assignment1/formose
 git pull
-mod -f dostuff.py
+mod -f doStuff.py
 cd ..

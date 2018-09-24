b4a68c96e9d36c618a7a259613afda8392c7d897
Mark Jervelund <mark@jervelund.com>
2018-09-24 17:37:19 +0200

    maybe it works now.

--- a1.sh
+++ a1.sh
@@ -4,9 +4,17 @@ export PATH=$MOD_INSTALL_PREFIX/bin:$PATH
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$MOD_INSTALL_PREFIX/lib/pkgconfig
 export LD_LIBRARY_PATH=/mnt/nfs/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
 
-
 cd /home/imada/mjerv15/courses/DM840/assignment1/formose
 git pull
 pwd
 mod -f doStuff.py
-cd ..
+scp summary/summary.pdf alzira:WWWpublic/dm840/
+
+
+
+
+
+
+
+
+ssh alzira 'chmod -R 777 WWWpublic/* '

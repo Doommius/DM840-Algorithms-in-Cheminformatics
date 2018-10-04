34153cf71002ed2e2c9be4f3a7f0cc5eaac710a1
Mark Jervelund <mark@jervelund.com>
2018-10-04 14:32:22 +0200

    Added the raindeer thing.

--- a1.sh
+++ a1.sh
@@ -12,6 +12,9 @@ mv summary/summary.pdf summary/formose.pdf
 scp summary/formose.pdf alzira:WWWpublic/dm840/
 rm summary/formose.pdf
 
+cd ../raindeer/
+mod -f raindeer.py
+
 cd ../catalan/
 sh solve.sh 1
 

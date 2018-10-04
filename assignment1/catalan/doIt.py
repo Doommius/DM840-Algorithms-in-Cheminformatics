caf5fb374ee8fd82dcc5cc0f708e0f290d8d461c
Mark Jervelund <mark@jervelund.com>
2018-10-04 14:30:32 +0200

    Added the raindeer thing.

--- assignment1/catalan/doIt.py
+++ assignment1/catalan/doIt.py
@@ -1,11 +1,11 @@
-# config.chem.checkMoleculeVerbose = False # don't touch this, or you will be spammed to death
+#config.chem.checkMoleculeVerbose = False # don't touch this, or you will be spammed to death
 dgPrinter = DGPrinter()
-dgPrinter.withGraphImages = False  # be careful, the derivation graphs can become quite large
+dgPrinter.withGraphImages =False # be careful, the derivation graphs can become quite large
 flowPrinter = DGFlowPrinter()
 flowPrinter.printUnfiltered = False
 config.ilp.solverVerbose = True
 
-g = graphDFS("[0]", "Goal")
+goal = graphDFS("[0]", "Goal")
 level = graphGML(levelFile, "Start")
 postSection("Loaded Graphs")
 for a in g: a.print()
@@ -26,7 +26,7 @@ postSection("Product Graphs")
 for a in dg.products: a.print() # this can take a while in the post processing
 
 flow = dgFlow(dg)
-flow.objectiveFunction = DGFlowLinExp() # important, otherwise the default function will min. #edgesUsed which may take a long time
+flow.objectiveFunction =  DGFlowLinExp() # important, otherwise the default function will min. #edgesUsed which may take a long time
 flow.addSource(level)
 flow.addSink(goal)
 flow.addConstraint(inFlow(level) == 1)

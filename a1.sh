535e3603b056c1f92dbe8568c6e55351a254ec15
Mark Jervelund <mark@jervelund.com>
2018-10-04 14:43:03 +0200

    Added the raindeer thing.

--- a1.sh
+++ a1.sh
@@ -4,9 +4,14 @@ export PATH=$MOD_INSTALL_PREFIX/bin:$PATH
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$MOD_INSTALL_PREFIX/lib/pkgconfig
 export LD_LIBRARY_PATH=/mnt/nfs/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
 
-cd /home/imada/mjerv15/courses/DM840/assignment1/formose
+cd /home/imada/mjerv15/courses/DM840/assignment1
 git pull
 pwd
+
+echo"----------------------------------------------------------"
+echo "beginning with Formose"
+echo"----------------------------------------------------------"
+cd formose/
 mod -f doStuff.py
 mv summary/summary.pdf summary/formose.pdf
 scp summary/formose.pdf alzira:WWWpublic/dm840/
@@ -14,12 +19,14 @@ rm summary/formose.pdf
 
 echo"----------------------------------------------------------"
 echo "done with formose"
+echo "beginning with Reindeer"
 echo"----------------------------------------------------------"
 
 cd ../reindeer/
 mod -f reindeer.py
 echo"----------------------------------------------------------"
 echo "done with Reindeer"
+echo "beginning with Catalan"
 echo"----------------------------------------------------------"
 
 cd ../catalan/

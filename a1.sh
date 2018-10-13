74b3c6aefab25da5424252ea788c79971bd4aa80
Mark Jervelund <mark@jervelund.com>
2018-10-13 23:04:21 +0200

    Updated some things for the catalan, and for the Reindeer

--- a1.sh
+++ a1.sh
@@ -8,42 +8,35 @@ cd /home/imada/mjerv15/courses/DM840/assignment1
 git pull
 pwd
 
+echo "#################################################################################################################"
+echo "beginning with Formose"
+echo "#################################################################################################################"
+cd formose/
+mod -f doStuff.py
+mv summary/summary.pdf summary/formose.pdf
+scp summary/formose.pdf alzira:WWWpublic/dm840/
+rm summary/formose.pdf
+
+echo "#################################################################################################################"
+echo "done with formose"
+echo "beginning with Reindeer"
+echo "#################################################################################################################"
+
+cd ../reindeer/
+mod -f reindeer.py
+mv summary/summary.pdf summary/reindeer.pdf
+scp summary/reindeer.pdf alzira:WWWpublic/dm840/
+rm summary/reindeer.pdf
+echo "#################################################################################################################"
+echo "done with Reindeer"
 echo "beginning with Catalan"
 echo "#################################################################################################################"
 
-cd catalan/
+cd ../catalan/
 sh solve.sh 1
 mv summary/summary.pdf summary/catalan.pdf
 scp summary/catalan.pdf alzira:WWWpublic/dm840/
 rm summary/catalan.pdf
-
-sh solve.sh 2
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
-rm summary/catalan2.pdf
-
-sh solve.sh 3
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
-rm summary/catalan3.pdf
-
-sh solve.sh 4
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
-rm summary/catalan4.pdf
-
-sh solve.sh 5
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
-rm summary/catalan5.pdf
-
-sh solve.sh 34
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
-rm summary/catalan34.pdf
-
-
-
 
 
 echo "#################################################################################################################"
---
+++ solvecatalan.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+export MOD_INSTALL_PREFIX=/mnt/nfs/Shared/jlandersen/shared
+export PATH=$MOD_INSTALL_PREFIX/bin:$PATH
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$MOD_INSTALL_PREFIX/lib/pkgconfig
+export LD_LIBRARY_PATH=/mnt/nfs/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
+
+cd /home/imada/mjerv15/courses/DM840/assignment1
+git pull
+pwd
+
+echo "beginning with Catalan"
+echo "#################################################################################################################"
+
+cd catalan/
+sh solve.sh 1
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan.pdf
+
+sh solve.sh 2
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan2.pdf
+
+sh solve.sh 3
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan3.pdf
+
+sh solve.sh 4
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan4.pdf
+
+sh solve.sh 5
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/ss
+rm summary/catalan5.pdf
+
+sh solve.sh 6
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan6.pdf
+
+sh solve.sh 7
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan6.pdf
+
+sh solve.sh 8
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan6.pdf
+
+sh solve.sh 34
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan34.pdf
+
+
+
+ssh alzira 'chmod -R 777 WWWpublic/* '
+
+echo "#################################################################################################################"
+echo "done with Catalan"
+echo "#################################################################################################################"
+
+

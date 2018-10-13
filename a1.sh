3c23d1b880a7c3734c0fa2e32ae495a5b5a0bda9
Mark Jervelund <mark@jervelund.com>
2018-10-13 22:58:52 +0200

    Updated some things for the catalan, and for the Reindeer

--- a1.sh
+++ a1.sh
@@ -17,6 +17,34 @@ mv summary/summary.pdf summary/catalan.pdf
 scp summary/catalan.pdf alzira:WWWpublic/dm840/
 rm summary/catalan.pdf
 
+sh solve.sh 2
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan2.pdf
+
+sh solve.sh 3
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan2.pdf
+
+sh solve.sh 4
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan2.pdf
+
+sh solve.sh 5
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan2.pdf
+
+sh solve.sh 34
+mv summary/summary.pdf summary/catalan.pdf
+scp summary/catalan.pdf alzira:WWWpublic/dm840/
+rm summary/catalan34.pdf
+
+
+
+
 
 echo "#################################################################################################################"
 echo "done with Catalan"

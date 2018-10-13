df082076ad5499c812ee7c7002555130eb018645
Mark Jervelund <mark@jervelund.com>
2018-10-13 23:13:57 +0200

    Updated some things for the catalan, and for the Reindeer

--- solvecatalan.sh
+++ solvecatalan.sh
@@ -18,39 +18,39 @@ scp summary/catalan.pdf alzira:WWWpublic/dm840/
 rm summary/catalan.pdf
 
 sh solve.sh 2
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
+mv summary/summary.pdf summary/catalan2.pdf
+scp summary/catalan2.pdf alzira:WWWpublic/dm840/
 rm summary/catalan2.pdf
 
 sh solve.sh 3
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
+mv summary/summary.pdf summary/catalan3.pdf
+scp summary/catalan3.pdf alzira:WWWpublic/dm840/
 rm summary/catalan3.pdf
 
 sh solve.sh 4
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
+mv summary/summary.pdf summary/catalan4.pdf
+scp summary/catalan4.pdf alzira:WWWpublic/dm840/
 rm summary/catalan4.pdf
 
 sh solve.sh 5
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/ss
+mv summary/summary.pdf summary/catalan5.pdf
+scp summary/catalan5.pdf alzira:WWWpublic/dm840/ss
 rm summary/catalan5.pdf
 
 sh solve.sh 6
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
+mv summary/summary.pdf summary/catalan6.pdf
+scp summary/catalan6.pdf alzira:WWWpublic/dm840/
 rm summary/catalan6.pdf
 
 sh solve.sh 7
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
-rm summary/catalan6.pdf
+mv summary/summary7.pdf summary/catalan7.pdf
+scp summary/catalan7.pdf alzira:WWWpublic/dm840/
+rm summary/catalan7.pdf
 
 sh solve.sh 8
-mv summary/summary.pdf summary/catalan.pdf
-scp summary/catalan.pdf alzira:WWWpublic/dm840/
-rm summary/catalan6.pdf
+mv summary/summary8.pdf summary/catalan8.pdf
+scp summary/catalan8.pdf alzira:WWWpublic/dm840/
+rm summary/catalan8.pdf
 
 sh solve.sh 34
 mv summary/summary.pdf summary/catalan.pdf

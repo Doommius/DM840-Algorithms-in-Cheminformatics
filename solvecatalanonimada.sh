7af33c6532371a1ff3427b0e15f6b4b30b6259db
Mark <Mark@jervelund.com>
2018-10-14 14:06:36 +0200

    add solver for imada

--- solvecatalanonimada.sh
+++ solvecatalanonimada.sh
@@ -8,7 +8,7 @@ echo "beginning with Catalan"
 echo "#################################################################################################################"
 cd assignment1/catalan
 for i in $(seq 1 56); do
-    sh solve.sh
+    sh solve.sh $i
     mv summary/summary.pdf summary/catalan$i.pdf
     scp summary/catalan$i.pdf alzira:WWWpublic/dm840/
     ssh alzira 'chmod -R 777 WWWpublic/* '

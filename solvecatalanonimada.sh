9e5ba60e3944ddda7edfa8ac82eef7aca38853bb
Mark <Mark@jervelund.com>
2018-10-14 14:01:44 +0200

    added solve on imada with multiple nodes.

--- solvecatalanonimada.sh
+++ solvecatalanonimada.sh
@@ -1,9 +1,16 @@
+export MOD_INSTALL_PREFIX=/mnt/nfs/Shared/jlandersen/shared
+export PATH=$MOD_INSTALL_PREFIX/bin:$PATH
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$MOD_INSTALL_PREFIX/lib/pkgconfig
+export LD_LIBRARY_PATH=/mnt/nfs/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
+
+
 echo "beginning with Catalan"
 echo "#################################################################################################################"
-
+cd assignment1/catalan
 for i in $(seq 1 56); do
-        shuf -n 1 hostlist
-
+    sh solve.sh
+    mv summary/summary.pdf summary/catalan$i.pdf
+    scp summary/catalan$i.pdf alzira:WWWpublic/dm840/
 done
 
 echo "#################################################################################################################"

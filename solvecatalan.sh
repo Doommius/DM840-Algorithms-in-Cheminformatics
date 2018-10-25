6dcb7606583d3e461fb58bc8b2eb99593b771b39
Mark Jervelund <mark@jervelund.com>
2018-10-25 15:30:38 +0200

    updated file

--- solvecatalan.sh
+++ solvecatalan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export MOD_INSTALL_PREFIX=/mnt/nfs/Shared/jlandersen/shared
+export MOD_INSTALL_PREFIX=/mnt/Shared/jlandersen/shared
 export PATH=$MOD_INSTALL_PREFIX/bin:$PATH
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$MOD_INSTALL_PREFIX/lib/pkgconfig
-export LD_LIBRARY_PATH=/mnt/nfs/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/mnt/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
 
 cd /home/imada/mjerv15/courses/DM840/assignment1
 git pull

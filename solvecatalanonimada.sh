3ab8545b3261977e5de655a80582a93115057956
Ilona Benko <ilben18@student.sdu.dk>
2018-10-29 00:27:51 +0100

    fixed run on imada

--- solvecatalanonimada.sh
+++ solvecatalanonimada.sh
@@ -7,12 +7,12 @@ echo "beginning with Catalan"
 echo "#################################################################################################################"
 cd assignment1/catalan
 
-ssh alzira 'mkdir dm/done'
+ssh alzira 'mkdir dm840/catalan'
 for i in $(seq 1 56); do
     sh solve.sh $i
-    ssh alzira 'mkdir dm840/done/$i'
-    scp summary/summary.pdf alzira:WWWpublic/dm840/$i
-    scp out alzira:WWWpublic/dm840/$i/out
+    ssh alzira 'mkdir dm840/catalan/$i'
+    scp summary/summary.pdf alzira:WWWpublic/dm840/catalan/$i
+    scp out alzira:WWWpublic/dm840/catalan/$i/out
     ssh alzira 'chmod -R 777 WWWpublic/* '
 done
 

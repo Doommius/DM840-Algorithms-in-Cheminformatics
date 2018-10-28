8fdd834161f1bdc90c6379f5c27dcb7595fb7110
Ilona Benko <ilben18@student.sdu.dk>
2018-10-29 00:41:19 +0100

    fixed run on imada

--- solvecatalanonimada.sh
+++ solvecatalanonimada.sh
@@ -10,7 +10,10 @@ cd assignment1/catalan
 
 ssh alzira 'mkdir WWWpublic/dm840/assignment1'
 ssh alzira 'mkdir WWWpublic/dm840/assignment1/catalan'
-for i in $(seq 1 56); do
+for i in $(seq 1 2); do
+    echo "#################################################################################################################"
+    echo "Starting with $i"
+    echo "#################################################################################################################"
     sh solve.sh $i
     ssh alzira 'mkdir WWWpublic/dm840/assignment1/catalan/$i'
     scp summary/summary.pdf alzira:WWWpublic/dm840/assignment1/catalan/$i/

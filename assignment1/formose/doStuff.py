ecbd9cbad80fed2c8c98bf1c64917eb3f7b5e92d
Mark Jervelund <mark@jervelund.com>
2018-10-29 18:06:26 +0100

    added things for a2 to test.

--- assignment1/formose/doStuff.py
+++ assignment1/formose/doStuff.py
@@ -8,44 +8,39 @@ flowPrinter = DGFlowPrinter()
 flowPrinter.printUnfiltered = False
 
 # Uncomment the following (and comment out line 23-26) in order to see node IDs
-graphLikePrinter = GraphPrinter()
-graphLikePrinter.withIndex = True
+ graphLikePrinter = GraphPrinter()
+ graphLikePrinter.withIndex = True
 
-molLikePrinter = GraphPrinter()
-molLikePrinter.setReactionDefault()
-molLikePrinter.withIndex = True
+ molLikePrinter = GraphPrinter()
+ molLikePrinter.setReactionDefault()
+ molLikePrinter.withIndex = True
+
+ postSection("Loaded Graphs")
+for a in g: a.print()
+postSection("Loaded Rules")
+ for a in r: a.print(graphLikePrinter, molLikePrinter)
 
 postSection("Loaded Graphs")
-for a in g: a.
-print()
+for a in inputGraphs: a.print()
 postSection("Loaded Rules")
-for a in r: a.
-print(graphLikePrinter, molLikePrinter)
-
-postSection("Loaded Graphs")
-for a in inputGraphs: a.
-print()
-postSection("Loaded Rules")
-for a in inputRules: a.
-print()
+for a in inputRules: a.print()
 
 # expand the chemical space, molecules having more than 5 carbon
 # atoms are not allowed to react (but might still be created)
 dg = dgRuleComp(inputGraphs,
-                addSubset(inputGraphs)
-                >> rightPredicate[lambda d: all(a.vLabelCount("C") <= 5 for a in d.right)](
-                    repeat(inputRules)
-                )
-                )
+	addSubset(inputGraphs)
+	>> rightPredicate[lambda d: all(a.vLabelCount("C") <= 5 for a in d.right)](
+		repeat(inputRules)
+	)
+)
 dg.calc()
 
-dg.
-print()
+dg.print()
 
 # Do the following only if you know that you do not have too many products.
 
-# postSection("All Vertex Graphs")
-# for a in dg.vertexGraphs: a.print()
+#postSection("All Vertex Graphs")
+#for a in dg.vertexGraphs: a.print()
 
 
 # Choose one of the two following options:
@@ -67,8 +62,7 @@ flowPathway.addConstraint(inFlow(formaldehyde) == 2)
 flowPathway.addConstraint(outFlow(glycolaldehyde) == 1)
 flowPathway.calc()
 flowPathway.solutions.list()
-flowPathway.solutions.
-print(flowPrinter)  # the argument is optional, see the top of the file for why we have it
+flowPathway.solutions.print(flowPrinter) # the argument is optional, see the top of the file for why we have it
 
 # A query for autocatalysis
 # This will, in a pre-processing step check if a compund
@@ -87,5 +81,4 @@ flowAutocata.solutions.list()
 # note however, that a solution _could_ use a molecule with 5 C atoms.
 # It's just for having a nicer figure.
 flowPrinter.dgPrinter.pushVertexVisible(lambda g, dg: g.vLabelCount("C") <= 4)
-flowAutocata.solutions.
-print(flowPrinter)  # the argument is optional
+flowAutocata.solutions.print(flowPrinter) # the argument is optional

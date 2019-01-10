86ac26b55052ac5776eecbc88a2b5654d91c02ac
Mark Jervelund <mark@jervelund.com>
2019-01-10 23:56:54 +0100

    More printing

--- assignment2/Option1_Atom-Atom-Mapping/main.cpp
+++ assignment2/Option1_Atom-Atom-Mapping/main.cpp
@@ -142,11 +142,7 @@ std::vector <std::shared_ptr<mod::rule::Rule>> doStuff(const std::vector <std::s
 
     } else if (reaction == 6) {
 
-        setById(0, 0);
-        setById(1, 1);
-        setById(6, 2);
-        setById(7, 3);
-        vertexMaps.push_back(vertexMap);
+       vertexMaps.push_back(vertexMap);
 
     } else if (reaction == 5) {
 

563e1c182478f8cc2e8722f4c384e6118654093c
Mark Jervelund <mark@jervelund.com>
2019-01-10 14:27:12 +0100

    Added git pull file

--- assignment2/Option1_Atom-Atom-Mapping/wrapper.sh
+++ assignment2/Option1_Atom-Atom-Mapping/wrapper.sh
@@ -4,7 +4,7 @@ export PATH=$MOD_INSTALL_PREFIX/bin:$PATH
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$MOD_INSTALL_PREFIX/lib/pkgconfig
 export LD_LIBRARY_PATH=/mnt/nfs/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
 
-
+cd /home/mjerv15/courses/DM840/assignment2/Option1_Atom-Atom-Mapping
 rm -rf out
 mkdir out
 make -j 2 && ./test $@ && mod_post

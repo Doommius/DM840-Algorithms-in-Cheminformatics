2c3f50fb6d029c005a612ee3b2999e441e422c55
Mark Jervelund <mark@jervelund.com>
2019-01-10 22:49:50 +0100

    More printing

--- assignment2/Option1_Atom-Atom-Mapping/main.cpp
+++ assignment2/Option1_Atom-Atom-Mapping/main.cpp
@@ -168,9 +168,9 @@ std::vector <std::shared_ptr<mod::rule::Rule>> doStuff(const std::vector <std::s
         setById(4, 8);
         setById(5, 9);
         //Third
-        setById(6, 14);
-        setById(8, 15);
-        setById(7, 16);
+//        setById(6, 14);
+//        setById(8, 15);
+//        setById(7, 16);
 
         vertexMaps.push_back(vertexMap);
 

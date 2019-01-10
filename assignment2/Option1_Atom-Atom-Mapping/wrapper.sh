9ad20010d8eb134c8a4c3696f140354f435a6383
Mark Jervelund <mark@jervelund.com>
2019-01-10 14:30:32 +0100

    They changed things again.

--- assignment2/Option1_Atom-Atom-Mapping/wrapper.sh
+++ assignment2/Option1_Atom-Atom-Mapping/wrapper.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-export MOD_INSTALL_PREFIX=/mnt/nfs/Shared/jlandersen/shared
+export MOD_INSTALL_PREFIX=/mnt/Shared/jlandersen/shared
 export PATH=$MOD_INSTALL_PREFIX/bin:$PATH
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$MOD_INSTALL_PREFIX/lib/pkgconfig
-export LD_LIBRARY_PATH=/mnt/nfs/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/mnt/Shared/jlandersen/shared/lib64:$LD_LIBRARY_PATH
+
+
 
 cd /home/mjerv15/courses/DM840/assignment2/Option1_Atom-Atom-Mapping
 rm -rf out
